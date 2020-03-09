fdf8cfd2ec5876e80a4e429fb564f47dab237255
zhizhuyun <zizhuyun97@gmail.com>
2020-03-09 11:32:58 +0800

    Site updated: 2020-03-09 11:32:56

--- js/custom.js
+++
@@ -1,6 +0,0 @@
-// 顶部进度条
-$(document).ready(function () {
-  $(window).scroll(function(){
-    $(".top-scroll-bar").attr("style", "width: " + ($(this).scrollTop() / ($(document).height() - $(this).height()) * 100) + "%; display: block;");
-  });
-});
